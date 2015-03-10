7b3532785c4dec78e853138572d3d8c75b73fe0c
huirongli <564291013@qq.com>
2015-03-10 16:43:41 +0800

    第二次commit 新建了类文件HelloGit.java

---
+++ HelloGit/src/com/dyr/test/HelloGit.java
@@ -0,0 +1,27 @@
+/*
+ * www.dyr.com
+ * Copyright (c) 2014 All Rights Reserved
+ */
+/**
+ * 
+ */
+package com.dyr.test;
+
+/**
+ * Project:HelloGit
+ * Packge:com.dyr.test
+ * FileName:HelloGit.java
+ * Comments:
+ * JDK Version:
+ * Author:Capser
+ * Create Date:2015-3-10 обнГ4:42:02
+ * Modified By:Capser
+ * Modified Time:
+ * What is Modified:
+ * Version:
+ */
+public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("Hello Git!!!");
+	}
+}
